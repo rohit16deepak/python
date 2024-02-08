17a310f6b61e8510acd540691e63afccda776647
Rohit <rvkillerbean@gmail.com>
2024-02-08 19:12:08 +0530

    modified

--- reverse.py
+++ reverse.py
@@ -1,12 +1,9 @@
-a=int(input())
+num=int(input())
 rev=0
-i=0
-while i<3:
-    z=a%10
-    rev= (rev*10)+z
-    z=0
-    a=a//10
-    i+=1
+while (num>0):
+    rem=num%10
+    num=num//10
+    rev= (rev*10)+rem
     
 print(rev)
 
